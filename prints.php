e2991bef08fb4064fc0a9f2e874e49f91c4b9839
akherr <akherr@vt.edu>
2016-01-06 17:04:39 -0500

    output formatting stuff

--- prints.php
+++ prints.php
@@ -2,9 +2,25 @@
 
 include 'dbconnect.php';
 
+$results = $db_connection->query( 'SELECT * from Suppliers' );
+
+echo '<table id ="resultstable"><tr>';
+
+$numfields = mysql_num_fields($results);
+
+for($i=0; $i<numfields; $i++)
+{
+    echo '<td>' . mysql_field_name($results, $i). '</td>';
+}
+?>
+
+<?php
+
 foreach ( $results as $row ) {
 	echo '<p id="quote">' . $row['testimonial'] . '</p>';
 	echo '<p id="author">&ndash;' . $row['author'] . '</p>';
 }
 
+echo '</table>';
 ?>
+
