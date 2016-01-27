7123560f66ada8b4d14f1eb26d9a5e69be003ec4
Austin Herr <austin.herr@gmail.com>
2016-01-27 14:46:47 -0500

    correct those html tags

--- prints.php
+++ prints.php
@@ -7,20 +7,20 @@ $results = $conn->query("SELECT * from Names");
 echo '<html><h2> Is this thing on?</h2></html>';
 $numfields = 2;
 
-echo '<html><p> Number of Fields: ' . $numfields . '</p></html>';
+echo '<html><p> Number of Fields: ' . $numfields . '</p>';
 echo '<table id ="resultstable"><tr>';
 
 
 
 for($i=0; $i<$numfields; $i++)
 {
-    echo '<html><p>Inside For Loop before fetch field</p></html>';
+    echo '<p>Inside For Loop before fetch field</p>';
     $fieldinfo = mysqli_fetch_field_direct($results, $i);
-    echo '<html><p>Inside For Loop before print field name</p></html>';
+    echo '<p>Inside For Loop before print field name</p>';
     echo '<td>' .$fieldinfo->name. '</td>';
 }
 
-echo '</tr>'
+echo '</tr></html>'
 ?>
 
 <?php
