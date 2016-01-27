f852cc24af66b517c8a2110fb070c6be5c8a2c4a
Austin Herr <austin.herr@gmail.com>
2016-01-27 15:07:40 -0500

    tablezzz

--- prints.php
+++ prints.php
@@ -8,16 +8,16 @@ echo '<html><h2> Is this thing on?</h2></html>';
 $numfields = 2;
 
 echo '<html><p> Number of Fields: ' . $numfields . '</p>';
-echo "<table id ='resultstable'><tr>";
+echo "<table border='1' id='resultstable'><tr>";
 
 
 
 for($i=0; $i<$numfields; $i++)
 {
-    echo '<p>Inside For Loop before fetch field</p>';
+    echo '<td><p>Inside For Loop before fetch field</p>';
     $fieldinfo = mysqli_fetch_field_direct($results, $i);
     echo '<p>Inside For Loop before print field name</p>';
-    echo '<p>' . $fieldinfo->name . '</p>';
+    echo '<p>' . $fieldinfo->name . '</p></td>';
 }
 
 echo '</tr></html>'
