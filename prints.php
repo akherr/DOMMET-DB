332771b16562f80d7f3e041fba4c7e04edfa3965
Austin Herr <austin.herr@gmail.com>
2016-01-27 14:34:23 -0500

    more commenting

--- prints.php
+++ prints.php
@@ -5,11 +5,12 @@ include 'dbconnect.php';
 $results = $conn->query("SELECT * from Names");
 
 echo '<html><h2> Is this thing on?</h2></html>';
-echo '<table id ="resultstable"><tr>';
-
 $numfields = 2;
 
-echo '<html><p> Number of Fields: ' . $numfields . '</p></html>';
+//echo '<html><p> Number of Fields: ' . $numfields . '</p></html>';
+//echo '<table id ="resultstable"><tr>';
+
+
 
 //for($i=0; $i<$numfields; $i++)
 //{
@@ -19,14 +20,15 @@ echo '<html><p> Number of Fields: ' . $numfields . '</p></html>';
     //echo '<td>' .$fieldinfo->name. '</td>';
 //}
 
-echo '</tr>'
+//echo '</tr>'
 ?>
 
 <?php
 
 while($row = $results->fetch_assoc()) {
-	echo '<p id="firstname">' . $row['first'] . '</p>';
-	echo '<p id="lastname">&ndash;' . $row['last'] . '</p>';
+    echo "<html><p> Inside fetch while";
+    echo '<p id="firstname">' . $row['first'] . '</p>';
+    echo '<p id="lastname">&ndash;' . $row['last'] . '</p>';
 }
 
 echo '</table>';
