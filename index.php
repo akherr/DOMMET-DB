ee8ceabd717c29982f37490e67b9e80583d206d2
akherr <akherr@vt.edu>
2016-03-01 18:17:52 -0500

    new print file

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <head> DomMet Contacts DB</head>
 <body>
     <p> This is a test of the DomMet Contacts DB Website </p>
-<form method="post" action="prints.php">
+<form method="get" action="prints.php">
       <input name="printsuppliers" type="submit" value="Get Suppliers"/>
 </form>
-<form method="post" action="prints1.php">
+<form method="get" action="prints1.php">
       <input name="printSupplierSpecific" type="submit" value="Get Suppliers Specific" />
       <input name="printSupplierstxt" type="text" />
 </form>      
@@ -17,7 +17,6 @@ echo
 //include ($_SERVER['DOCUMENT_ROOT']."prints.php");
 
 ini_set('date.timezone','America/New_York');
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
---
+++ prints1.php
@@ -0,0 +1,31 @@
+<?php
+include ($_SERVER['DOCUMENT_ROOT']."dbconnect.php");
+$results = $conn->query("SELECT * from Suppliers where Name LIKE '%" . $_GET["printSupplierstxt"] . "%'");
+$numfields = $conn->field_count;
+
+echo '<html><p> Number of Fields: ' . $numfields . '</p>';
+echo "<table border='1' id='resultstable'><tr>";
+
+for($i=0; $i<$numfields; $i++)
+{
+    $fieldinfo = mysqli_fetch_field_direct($results, $i);
+    echo "<td id='tableheaders'>" . $fieldinfo->name . "</td>";
+}
+
+echo '</tr>';
+
+while($row = $results->fetch_assoc()) {
+    echo '<tr><td>' . $row['SupplierID'] . '</td>';
+    echo '<td>' . $row['Name'] . '</td>';
+    echo '<td>' . $row['Address'] . '</td>';
+    echo '<td>' . $row['City'] . '</td>';
+    echo '<td>' . $row['State'] . '</td>';
+    echo '<td>' . $row['Zipcode'] . '</td>';
+    echo '<td>' . $row['Fax'] . '</td>';
+    echo '<td>' . $row['Website'] . '</td>';
+    echo '<td>' . $row['Email'] . '</td></tr>';
+    
+}
+
+echo '</table></html>';
+?>
