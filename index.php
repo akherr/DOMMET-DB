d7d78e0e35b57d6e32adf9c0bacb8aac686df445
akherr <akherr@vt.edu>
2016-02-29 20:44:12 -0500

    hm

--- index.php
+++ index.php
@@ -12,8 +12,6 @@ include 'prints.php';
 
 <?php
   ini_set('date.timezone','America/New_York');
-  echo "Last modified: ", date("Y.M.d (D) H:i.", filemtime(__FILE__)), "<br />\n";
-  error_reporting (E_ALL|E_WARNING|E_PARSE);
   show_source(__FILE__);
 ?>
 </body>
