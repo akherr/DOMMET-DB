bb839fbba6608607eb395d94053ef5bb2779e781
akherr <akherr@vt.edu>
2016-03-02 12:55:23 -0500

    fsdoij

--- prints.php
+++ prints.php
@@ -3,7 +3,7 @@
 include ($_SERVER['DOCUMENT_ROOT']."dbconnect.php");
 $results = $conn->query("SELECT * from Suppliers where Name LIKE 's%'");
 $numfields = $conn->field_count;
-$numrows = $conn->query("SELECT COUNT(*) from Suppliers where name LIKE 's%'");
+$numrows = $conn->query("SELECT COUNT(SupplierID) from Suppliers where name LIKE 's%'");
 
 echo '<html><p> Number of Fields: ' . $numfields . ' Number of rows: ' . $numrows[0] . '</p>';
 echo "<table border='1' id='resultstable'><tr>";
