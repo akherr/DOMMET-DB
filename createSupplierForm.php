b3b29ff00c9b06ddbdbbbe48781ca09d00cdde64
akherr <akherr@vt.edu>
2016-03-08 17:11:16 -0500

    form fields adding

--- createSupplierForm.php
+++ createSupplierForm.php
@@ -4,14 +4,79 @@
     <head></head>
 <body>    
 <form method="post" action="submitNewSupplier.php"
-    <div>
-        <p>Supplier Name: </p>
-        <input name="supName" type="text"></input>
-    </div>
-    <div>
-        <p>Address: </p>
-        <input name="supAddress" type="text"></input>
-    </div>
+    <p>
+        Supplier Name:
+        <input name="supName" type="text"></input><br />
+        Address:
+        <input name="supAddress" type="text"></input><br />
+        City:
+        <input name="supCity" type="text"></input><br />
+        State:
+        <select name="supState">
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+        </select><br />
+        Zipcode:
+        <input name="supZipcode" type="text"></input><br />
+        Fax:
+        <input name="supFax" type="text"></input><br />
+        Website:
+        <input name="supWebsite" type="text"></input><br />
+        Email:
+        <input name="supEmail" type="text"></input><br />
+        <input name="submitCreateSupplier" type="submit"></input>
+    </p>
+</form>
+        
 <?php
 
 ?>
