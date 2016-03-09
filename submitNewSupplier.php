fb2aafde6e90ad9262958fe8cb2ce15ffb3fcbd3
akherr <akherr@vt.edu>
2016-03-09 13:57:53 -0500

    new supplier form

--- submitNewSupplier.php
+++ submitNewSupplier.php
@@ -1,9 +1,12 @@
 <?php
+include ($_SERVER['DOCUMENT_ROOT']."dbconnect.php");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$sql = "insert into Suppliers (Name, Address, City, State,"
+        . " Zipcode, Fax, Website, Email) "
+        . "values('$_POST[supName]', '$_POST[supAddress]', '$_POST[supCity]',"
+        . "'$_POST[supState]', '$_POST[supZipcode]', '$_POST[supFax]', '$_POST[supWebsite]',"
+        . "'$_POST[supEmail]')";
 
+$result = $conn->query($sql);
+echo "<html><p>$result</p></html>";
 ?>
